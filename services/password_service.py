834fd140655ca19c3183da20596cde70b11ef12a
lievlll <anfaloway@gmail.com>
2026-05-04 22:59:41 +0300

    Update password_service.py

--- services/password_service.py
+++ services/password_service.py
@@ -3,12 +3,11 @@ import string
 
 class PasswordService:
 
-    def generate_password(self, length=8, use_digits=True, use_symbols=True):
-        chars = string.ascii_letters
+    def generate(self, length):
+        chars = string.ascii_letters + string.digits
+        password = ""
 
-        if use_digits:
-            chars += string.digits
-        if use_symbols:
-            chars += string.punctuation
+        for i in range(length):
+            password += random.choice(chars)
 
-        return ''.join(random.choice(chars) for _ in range(length))
+        return password
