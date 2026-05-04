423c7dd203cec46f3b01219ea23fbbc69e1a1e7a
lievlll <anfaloway@gmail.com>
2026-05-04 22:51:38 +0300

    Create test_passwords.py

---
+++ tests/test_passwords.py
@@ -0,0 +1,22 @@
+
+import unittest
+from services.password_service import PasswordService
+
+class TestPassword(unittest.TestCase):
+
+    def test_length(self):
+        service = PasswordService()
+        pwd = service.generate_password(10)
+        self.assertEqual(len(pwd), 10)
+
+    def test_not_empty(self):
+        service = PasswordService()
+        pwd = service.generate_password()
+        self.assertTrue(len(pwd) > 0)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
