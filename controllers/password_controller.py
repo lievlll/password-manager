810b5b7ed7fac61222f89988865518f2bc13b76c
lievlll <anfaloway@gmail.com>
2026-05-04 22:58:49 +0300

    Update password_controller.py

--- controllers/password_controller.py
+++ controllers/password_controller.py
@@ -1,23 +1,23 @@
+import datetime
 from models.password_record import PasswordRecord
 
 class PasswordController:
-    def __init__(self, storage, password_service):
+
+    def __init__(self, storage):
         self.storage = storage
-        self.password_service = password_service
         self.records = self.storage.load()
 
-    def add_record(self, service, username, password):
-        record = PasswordRecord.create(service, username, password)
+    def add(self, service, username, password):
+        now = str(datetime.datetime.now())
+        record = PasswordRecord(service, username, password, now)
         self.records.append(record)
 
-    def delete_record(self, index):
-        if 0 <= index < len(self.records):
-            self.records.pop(index)
-            return True
-        return False
-
-    def list_records(self):
+    def show_all(self):
         return self.records
+
+    def delete(self, index):
+        if index >= 0 and index < len(self.records):
+            self.records.pop(index)
 
     def save(self):
         self.storage.save(self.records)
